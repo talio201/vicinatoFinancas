0e5d91e4a9147a8618a0bc576894375de5a66cdf
talio201 <talio_fsa@hotmail.com>
2025-08-10 22:50:07 -0300

    feat(build): Otimiza o tamanho do build do frontend com code splitting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,23 @@ export default defineConfig({
     UnoCSS(),
     react(),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'react-vendor';
+            }
+            if (id.includes('@tanstack')) {
+              return 'tanstack-vendor';
+            }
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
